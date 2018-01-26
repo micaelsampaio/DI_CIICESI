9e22fce91238c279bbd9cd83aae82cca3c0a3d1e
Micael Sampaio <ruimicaelsampaio@gmail.com>
2018-01-26 00:14:41 +0000

    projetos pages

--- scripts/routes.js
+++ scripts/routes.js
@@ -4,18 +4,5 @@ app.config(['$routeProvider', '$locationProvider',
             .when("/", {
                 templateUrl: "main.html"
             })
-            .when("/noticias", {
-                templateUrl: "news.html"
-            })
-            .when("/investigadores", {
-                templateUrl: "investigadores.html"
-            })
-            .when("/eventos", {
-                templateUrl: "events.html"
-            })
-            .when("/bolsas", {
-                templateUrl: "bolsas.html"
-            })
-
         $locationProvider.html5Mode(true);
     }]);
